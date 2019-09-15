b760f40c3d430bda5a50909f5bc8722a2e9a92b9
sachin.pink <sachinzagade59@gmail.com>
2019-09-15 21:37:44 +0530

    added beforeclass annotation

--- MercuryTours/src/main/java/com/tours/Base/Baseclass.java
+++ MercuryTours/src/main/java/com/tours/Base/Baseclass.java
@@ -5,20 +5,21 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
 public class Baseclass 
 {
 	public WebDriver driver;
 	
-	@Test
+	
 	@Parameters("browser")
+	@BeforeClass
 	public void setup(String browser)
 	{
 		if(browser.equals("chrome"))
 		{
-			System.setProperty("webdriber.chrome.driver","C:\\Users\\sachin\\git\\MercuryTours\\MercuryTours\\drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\sachin\\git\\MercuryTours\\MercuryTours\\drivers\\chromedriver.exe");
 			WebDriver driver= new ChromeDriver();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			driver.get("http://newtours.demoaut.com/");
