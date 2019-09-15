7fd860203f75f3d91c00503c288405179c17b2a8
sachin.pink <sachinzagade59@gmail.com>
2019-09-15 21:25:57 +0530

    edited login page test

--- MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
+++ MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
@@ -1,5 +1,4 @@
 package com.tours.test;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.tours.Base.Baseclass;
@@ -8,12 +7,6 @@ import com.tours.UiPages.LoginPage;
 public class LoginPageTest extends Baseclass
 {
 	Baseclass baseclass=null;
-   
-	@BeforeMethod
-    public void setup()
-    {
-    	
-    }
 	
 	@Test
 	public void login()
