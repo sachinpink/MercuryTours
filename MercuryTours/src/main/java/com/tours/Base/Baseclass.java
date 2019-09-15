62a55b3c181c25d3ef258da955b3a9b0bee18b72
sachin.pink <sachinzagade59@gmail.com>
2019-09-15 21:23:49 +0530

    added chrome driver folder

--- MercuryTours/src/main/java/com/tours/Base/Baseclass.java
+++ MercuryTours/src/main/java/com/tours/Base/Baseclass.java
@@ -3,19 +3,33 @@ package com.tours.Base;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class Baseclass 
 {
 	public WebDriver driver;
 	
-	@BeforeClass
-	public void setup()
+	@Test
+	@Parameters("browser")
+	public void setup(String browser )
 	{
+		if(browser.equals("chrome"))
+		{
+			System.setProperty("webdriber.chrome.driver","C:\\Users\\sachin\\git\\MercuryTours\\MercuryTours\\drivers\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get("http://newtours.demoaut.com/");
+		}
+		else
+		{
+		
 		driver= new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://newtours.demoaut.com/");
+		}
 	}
 	
 
--- MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
+++ MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
@@ -1,4 +1,5 @@
 package com.tours.test;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.tours.Base.Baseclass;
@@ -6,7 +7,14 @@ import com.tours.UiPages.LoginPage;
 
 public class LoginPageTest extends Baseclass
 {
+	Baseclass baseclass=null;
+   
+	@BeforeMethod
+    public void setup()
+    {
     	
+    }
+	
 	@Test
 	public void login()
 	{
