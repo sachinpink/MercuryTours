782955bbd7cfd2b3711c44f061dee8a3e6f193ab
sachin.pink <sachinzagade59@gmail.com>
2019-09-16 10:08:22 +0530

    modified base class

--- MercuryTours/src/main/java/com/tours/Base/Baseclass.java
+++ MercuryTours/src/main/java/com/tours/Base/Baseclass.java
@@ -5,33 +5,37 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
 
 public class Baseclass 
 {
 	public WebDriver driver;
 	
-	
-	@Parameters("browser")
+	String browsers
+	="firefox";
 	@BeforeClass
-	public void setup(String browser)
+	public void setup()
 	{
-		if(browser.equals("chrome"))
+		if(browsers.equals("chrome"))
 		{
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\sachin\\git\\MercuryTours\\MercuryTours\\drivers\\chromedriver.exe");
-			WebDriver driver= new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.get("http://newtours.demoaut.com/");
+			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") +"\\drivers\\chromedriver.exe");
+			driver= new ChromeDriver();
 		}
 		else
 		{
 		
-		driver= new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("http://newtours.demoaut.com/");
+		  driver= new FirefoxDriver();
+		
 		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.get("http://newtours.demoaut.com/");
 	}
 	
-
+	@AfterClass
+	public void tearDown()
+	{
+		driver.close();
+	}
 }
--- MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
+++ MercuryTours/src/test/java/com/tours/test/LoginPageTest.java
@@ -1,4 +1,5 @@
 package com.tours.test;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.tours.Base.Baseclass;
@@ -6,13 +7,23 @@ import com.tours.UiPages.LoginPage;
 
 public class LoginPageTest extends Baseclass
 {
-	Baseclass baseclass=null;
-	
-	@Test
-	public void login()
+	LoginPage login_page;
+			
+	@Test(dataProvider="logindetails")
+	public void login( String un,String pass) throws InterruptedException
 	{
-		LoginPage login_page= new LoginPage(driver);
-		login_page.enterloginDetails("sachin","pass");
+	    login_page= new LoginPage(driver);
+		login_page.enterloginDetails(un,pass);
+		Thread.sleep(3000);
+	}
+	
+	@DataProvider(name="logindetails")
+	public Object[][] getData()
+	{
+		Object data[][]= new Object[1][2];
+		data[0][0]="sachin_pink";
+		data[0][1]="9172362161";
+		return data;
 	}
 
 }
